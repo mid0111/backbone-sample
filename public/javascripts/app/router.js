f7aad354407f7b59c7968dc4b91cdfbe40ae6557
mid0111 <m.tajima0111@gmail.com>
2013-11-28 19:04:57 +0900

    routerにちょくせつAppView定義してたのやめた。

--- public/javascripts/app/router.js
+++ public/javascripts/app/router.js
@@ -1,10 +1,7 @@
 define(function(require) {
-  var $ = require('jquery');
   var _ = require('underscore');
   var Backbone = require('backbone');
-  var Addresses = require('app/addressbook/collection');
-  var Address = require('app/addressbook/model');
-  var AddressView = require('app/addressbook/view');
+  var AppView = require('app/main');
 
   var Router = Backbone.Router.extend({
 
@@ -13,48 +10,6 @@ define(function(require) {
     },
     
     home: function() {
-      var AppView = Backbone.View.extend({
-        el: $('#app'),
-
-        events: {
-          'keypress #new-address': 'keyPress',
-          'click #delete-all': 'deleteAll'
-        },
-
-        initialize: function() {
-          this.input = this.$('#new-Address');
-          Addresses.bind('add', this.add, this);
-          Addresses.bind('reset', this.addAll, this);
-          Addresses.fetch();
-        },
-
-        add: function(address) {
-          var view = new AddressView({model: address});
-          this.$('#list').append(view.render().el);
-        },
-        
-        addAll: function() {
-          Address.each(this.add);
-        },
-
-        keyPress: function(e) {
-          if(e.keyCode === 13) {
-            Addresses.create({name: this.input.val()});
-            this.input.val('');
-          }
-        },
-
-        deleteAll: function(e) {
-          while(true) {
-            var address = Addresses.first();
-            if(address == null || address === 'undefined') {
-              break;
-            }
-            address.destroy();
-          }
-        }
-      });
-
       var App = new AppView;
     }
   });
